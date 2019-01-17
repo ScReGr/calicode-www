930e28f95c57b8c5cdb1b16b7e808374fddbf50c
Sunsetly <sunsetly2@gmail.com>
2019-01-17 18:18:11 -0500

    Alpha v0.0.3
    Big update!
    • Menu bar
    - Adjustable for desktop or mobile
    - Search bar
    - Menu items

--- script.js
+++ script.js
@@ -3,3 +3,11 @@
 
 // prints "hi" in the browser's dev tools console
 console.log('hi');
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+    
+    mainNav.classList.toggle('active');
+});
