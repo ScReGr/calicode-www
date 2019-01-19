94e5e36e16b02ba27e7ce0e6bb7d336ff30a7f40
Joshua Herron <herronjo110@live.com>
2019-01-19 18:07:08 -0500

    Make icons local, and remove some unneeded Javascript

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
-console.log('hi');
-
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
-
-navBarToggle.addEventListener('click', function () {
-    
-    mainNav.classList.toggle('active');
-});
+window.onload = function() {
+	let mainNav = document.getElementById('js-menu');
+	let navBarToggle = document.getElementById('js-navbar-toggle');
+	navBarToggle.addEventListener('click', function () {
+		mainNav.classList.toggle('active');
+	});
+}
