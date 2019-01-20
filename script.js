53a52d7d88ee80b663750cb703ac2be4ac9237d6
Sunsetly <sunsetly2@gmail.com>
2019-01-19 23:00:32 -0500

    Alpha v0.0.5

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-window.onload = function() {
-	let mainNav = document.getElementById('js-menu');
-	let navBarToggle = document.getElementById('js-navbar-toggle');
-	navBarToggle.addEventListener('click', function () {
-		mainNav.classList.toggle('active');
-	});
-}
+placeholder
