1d9c635c4c8aeb16bd249931454ccbd53557db1b
Joshua Herron <herronjo110@live.com>
2019-02-07 17:11:18 -0500

    Did a couple more API magic
    And also fixed some click issues where clicking on buttons and such brought you to the top of the page again.

--- profile.js
+++ profile.js
@@ -1,3 +1,10 @@
+var replyingid = -1;
+function replyto(user, id) {
+	document.getElementById("comment-input").value = "@"+user+" ";
+	replyingid = id;
+	document.getElementById("comment-input").focus();
+}
+
 window.onload = function() {
 	var id = getAllUrlParams().id;
 	document.getElementById("username").innerHTML = id;
@@ -9,7 +16,25 @@ window.onload = function() {
 		if (response.status == "error") {
 			document.getElementById("username").innerHTML = "UDNE"
 		} else {
+			document.getElementById("followButton").onclick = function(e) {
+				if (localStorage.sess != undefined) {
+					var xhr = new XMLHttpRequest();
+					xhr.open("POST","https://api.calicode.gq/follow.sjs",true);
+					xhr.send("sess"+localStorage.sess+"&id="+id);
+					xhr.onload = function(e2) {
+						location.reload();
+					}
+				}
+			}
 			document.getElementById("aboutUser").innerHTML = response.about;
+			document.getElementById("country").innerHTML = response.country;
+			document.getElementById("comments").innerHTML = "";
+			document.getElementById("numberFollowers").innerHTML = response.followers.length;
+			document.getElementById("numberFollowing").innerHTML = response.following.length;
+			var comments = response.comments;
+			for (var c in comments) {
+				document.getElementById("comments").innerHTML = document.getElementById("comments").innerHTML+'<div><div class="smallProfileIcon"></div><h4 style="margin-left: calc(35px + 1em);margin-bottom:0px;margin-top:calc(-35px - 1em)">'+comments[c].poster+'</h4><p id="commentContent">'+comments[c].content+'</p><div id="newButton" style="margin-left:calc(35px + 1em)"><a href="javascript:replyto(\''+comments[c].poster+'\','+c+')" style="padding: 0px 5px 0px 5px">Reply</a></div><a id="newButton" href="javascript:like('+c+')"><img src="assets/heart.svg" style="height:9px"></a></div>';
+			}
 		}
 	}
 }
